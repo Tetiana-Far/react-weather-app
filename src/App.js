a5a35e07235b1456bb9cac6ebb15a57e3b8f1828
Tetiana-Far <155846665+Tetiana-Far@users.noreply.github.com>
2025-01-18 22:02:59 +0300

    added external links

--- src/App.js
+++ src/App.js
@@ -9,9 +9,30 @@ function App() {
         <Weather defaultCity="Kyiv" />
         <footer className="mt-3 text-center">
           <p>
-            This project was coded by <a href="/">Tetiana Farafonova</a>, is{" "}
-            <a href="/">open-sourced on GitHub</a> and{" "}
-            <a href="/">hosted on Netlify</a>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/Tetiana-Far"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              Tetiana Farafonova
+            </a>
+            , is{" "}
+            <a
+              href="https://github.com/Tetiana-Far/react-weather-app"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://gentle-snickerdoodle-35cef9.netlify.app/"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
           </p>
         </footer>
       </div>
